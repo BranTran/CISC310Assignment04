9eeef73417cf1500a39196e77d9046b9ad5afcd0
BranTran <tran2833@stthomas.edu>
2020-04-16 11:58:40 -0500

    Begin long road of robust code

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,8 @@
 #include "pagetable.h"
 
 void printStartMessage(int page_size);
+std::vector<std::string> splitString(std::string text, char d);
+bool isNonNegativeInteger(char* pointer);
 
 int main(int argc, char **argv)
 {
@@ -15,11 +17,28 @@ int main(int argc, char **argv)
     }
 
     // Print opening instuction message
-    int page_size = std::stoi(argv[1]);
-    //Error check the page_size
+    // int page_size = std::stoi(argv[1]);
     //-Make sure it is a number (assignment 1)
-    //-Make sure that it is a power of 2 up to 32718
-    //  +while loop with left shift (<<)
+    char* size_input;
+    int page_size;
+    size_input = argv[1];
+    if(!isNonNegativeInteger(size_input)){
+      fprintf(stderr, "Error: entered page size is not a number %s\n",size_input);
+      return 1;
+    }
+    
+    page_size = atoi(size_input);
+    //-Make sure that it is a power of 2 up to 32768
+    int power_two = 1024;
+    while(page_size != power_two && power_two <= 32768){
+      power_two = power_two << 1;//Next power of two
+      //printf("Power of two is: %d\n", power_two);
+    }
+    if(power_two > 32768 || page_size != power_two){
+      fprintf(stderr, "Error: entered page size is not a power of two %d\n",page_size);
+      return 1;
+    }
+      
     printStartMessage(page_size);
 
     // Create physical 'memory'
@@ -33,15 +52,60 @@ int main(int argc, char **argv)
         // Handle command
         // TODO: implement this!
         // Parse the command on spaces (from assignment 2)
+      std::vector<std::string> argv = splitString(command,' ');
+      int argc = argv.size();
+      std::string argument;
+      argument = argv[0];
       
         // create <text_size> <data_size>
+      if(argument.compare("create") == 0){
+	//Verify create input parameters
+	if(argc!=3){
+	  fprintf(stderr,"Error: %s does not have the right number of arguments: %s <text_size> <data_size>\n",argument,argument);
+	}
+	//Validate create inputs
+	char* text_try = const_cast<char*>(argv[1].c_str());
+	if(!isNonNegativeInteger(text_try)){
+	  fprintf(stderr, "Error: entered text_size is not a number %s\n",text_try);
+	  return 1;
+	}
+	int text_size = atoi(text_try);
+	//int data_size = atoi(argv[2]);
+      }
         // allocate <PID> <var_name> <data_type> <number of elements>
+      else if(argument.compare("allocate") == 0){
+	if(argc!=5){
+	  fprintf(stderr,"Error: %s does not have the right number of arguments: %s <PID> <var_size> <data_type> <number of elements>\n",argument,argument);
+	}
+      }
         // set <PID> <var_name> <offset> <value_0> <value_1> ... <value_N>
+      else if(argument.compare("set") == 0){
+	if(argc < 5){
+	  fprintf(stderr,"Error: %s does not have the right number of arguments: %s <PID> <var_name> <offset> <value_0> [<value_1> ...]\n",argument,argument);
+	}
+      }
         // free <PID> <var_name>
+      else if(argument.compare("free") == 0){
+	if(argc!=3){
+	  fprintf(stderr,"Error: %s does not have the right number of arguments: %s <PID> <var_name>\n",argument,argument);
+	}
+      }
         // terminate <PID>
+      else if(argument.compare("terminate") == 0){
+	if(argc!=2){
+	  fprintf(stderr,"Error: %s does not have the right number of arguments: %s <PID>\n",argument,argument);
+	}
+      }
         // print <object>
+      else if(argument.compare("print") == 0){
+	if(argc!=2){
+	  fprintf(stderr,"Error: %s does not have the right number of arguments: %s <object>\n",argument,argument);
+	}
+      }
         // otherwise send an error
-      
+      else{
+	printf("Error: command not found %s\n",command.c_str());
+      }
         // Get next command
         std::cout << "> ";
         std::getline (std::cin, command);
@@ -66,4 +130,30 @@ void printStartMessage(int page_size)
     std::cout << "    * if <object> is \"processes\", print a list of PIDs for processes that are still running" << std:: endl;
     std::cout << "    * if <object> is a \"<PID>:<var_name>\", print the value of the variable for that process" << std:: endl;
     std::cout << std::endl;
+}
+
+bool isNonNegativeInteger(char* pointer){
+  if(std::string(std::to_string(atoi(pointer))).length() < std::string(pointer).length() || pointer == NULL || !isdigit(pointer[0])){
+    return false;
+  }
+  return true;
+}
+
+std::vector<std::string> splitString(std::string text, char d)
+{
+  std::vector<std::string> result;
+  int found = text.find_first_of(d,0);
+  std::string before;
+  std::string after;
+  before = text.substr(0, found);
+  after = text.substr(found+1, text.length());
+  while(found != std::string::npos)
+    {
+      result.push_back(before);
+      found = after.find_first_of(d,0);
+      before = after.substr(0, found);
+      after = after.substr(found+1, text.length());
+    }
+  result.push_back(after);
+  return result;
 }
